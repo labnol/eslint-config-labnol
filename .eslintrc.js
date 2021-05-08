d90ba5d98b2311384239d443a6762aab8930e310
Amit Agarwal <amit@labnol.org>
2021-05-08 12:45:12 +0530

    Dep updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -54,7 +54,8 @@ module.exports = {
       1,
       {
         ignoreRestSiblings: true,
-        argsIgnorePattern: 'res|next|^err|^ignore',
+        argsIgnorePattern: 'res|next|^err|^ignore|^_',
+        caughtErrors: 'none',
       },
     ],
   },
